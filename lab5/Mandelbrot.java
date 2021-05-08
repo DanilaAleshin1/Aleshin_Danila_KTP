c67a80885e05f832625bdb8612be1242775c21f0
Danila <wion201381@gmail.com>
2021-05-08 18:24:17 +0300

    lab

--- lab5/Mandelbrot.java
+++ lab5/Mandelbrot.java
@@ -2,7 +2,7 @@ import java.awt.geom.Rectangle2D;
 
 public class Mandelbrot extends FractalGenerator {
     public static final int MAX_ITERATIONS = 2000;
-        public String toString()
+    public String toString()
     {
         return "Mandelbrot";
     }
